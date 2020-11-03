c8a76f9b6ab740cdebb96a27003c2b63a5023914
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-11-03 16:19:35 +0300

    Fix quotes

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -6,23 +6,23 @@ from places.models import Place
 
 def show_index(request):
     places_geojson = {
-      "type": "FeatureCollection",
-      "features": []
+        'type': 'FeatureCollection',
+        'features': []
     }
     all_places = Place.objects.all()
 
     for place in all_places:
         details_url = reverse('place_info', args=[place.id])
         places_geojson['features'].append({
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [place.lng, place.lat]
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.lng, place.lat]
             },
-            "properties": {
-                "title": place.short_title,
-                "placeId": place.place_id,
-                "detailsUrl": details_url,
+            'properties': {
+                'title': place.short_title,
+                'placeId': place.place_id,
+                'detailsUrl': details_url,
             }
         })
     data = {'places_geojson': places_geojson}
